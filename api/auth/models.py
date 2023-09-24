6bce149193ece103153e0f2e568a7826654cd31c
gutrapp <trappgustavo@gmail.com>
2023-09-24 10:38:08 -0300

    feat: model declarations

--- api/auth/models.py
+++ api/auth/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import UserManager
+from clinic.models import Clinic, Address, Cellphone
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    PERSON = "P"
+    ADMIN = "A"
+    ROLE_CHOICES = [(PERSON, "Pessoa"), (ADMIN, "Administrador")]
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100, unique=True)
+    password = models.CharField(max_length=255)
+    type = models.CharField(max_length=1, choices=ROLE_CHOICES)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+
+class Admin(models.Model):
+    GLOBAL = "G"
+    CLINIC = "C"
+    GROUP_CHOICES = [
+        (GLOBAL, "Global"),
+        (CLINIC, "Clínica"),
+    ]
+
+    ADMIN = "A"
+    OPERATOR = "O"
+    ROLE_CHOICES = [
+        (ADMIN, "Administrador"),
+        (OPERATOR, "Operador"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    group = models.CharField(max_length=2, choices=GROUP_CHOICES)
+    role = models.CharField(max_length=2, choices=ROLE_CHOICES)
+    clinic = models.ManyToManyField(Clinic, blank=True)
+
+
+class Person(models.Model):
+    MALE = "M"
+    FEMALE = "F"
+    SEX_CHOICES = [
+        (MALE, "Masculino"),
+        (FEMALE, "Feminino"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+    cpf = models.CharField(max_length=12, unique=True)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=2, choices=SEX_CHOICES)
+    address = models.ManyToManyField(Address, blank=True)
+    cellphone = models.ManyToManyField(Cellphone, blank=True)
--- api/tests/models.py
+++ api/tests/models.py
@@ -1,3 +1,105 @@
+from datetime import datetime, timedelta
+
 from django.db import models
 
-# Create your models here.
+from auth.models import Person
+from clinic.models import Clinic
+
+
+class Result(models.Model):
+    MBTI = "MB"
+    SELF_KNOWLEDGE = "SK"
+    LOVE_LANGUAGE = "LO"
+    LIFE = "LI"
+    TESTS = [
+        (MBTI, "MBTI"),
+        (SELF_KNOWLEDGE, "Auto Conhecimento"),
+        (LOVE_LANGUAGE, "Linguagem Amorosa"),
+        (LIFE, "Vida"),
+    ]
+
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    expired = models.BooleanField(default=False)
+    testTaken = models.BooleanField(default=False)
+    test = models.CharField(choices=TESTS, max_length=2)
+    key = models.CharField(max_length=50, unique=True)
+    seen = models.BooleanField(default=False)
+    clinic = models.ForeignKey(Clinic, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True, null=True, blank=True)
+    expires_at = models.DateField(
+        default=datetime.now() + timedelta(days=30), null=True, blank=True
+    )
+
+
+class Life(models.Model):
+    average = models.DecimalField(max_digits=5, decimal_places=2)
+    total = models.IntegerField()
+    result = models.OneToOneField(Result, on_delete=models.CASCADE)
+
+
+class SelfKnowledge(models.Model):
+    VISUAL = "VI"
+    KINESTHETIC = "KI"
+    AUDITORY = "AU"
+    DIGITAL = "DI"
+    TRAITS = [
+        (VISUAL, "Visual"),
+        (KINESTHETIC, "Cinestésico"),
+        (AUDITORY, "Auditório"),
+        (DIGITAL, "Digital"),
+    ]
+
+    first = models.CharField(choices=TRAITS, max_length=2)
+    second = models.CharField(choices=TRAITS, max_length=2)
+    third = models.CharField(choices=TRAITS, max_length=2)
+    fourth = models.CharField(choices=TRAITS, max_length=2)
+    firstScore = models.DecimalField(max_digits=5, decimal_places=2)
+    secondScore = models.DecimalField(max_digits=5, decimal_places=2)
+    thirdScore = models.DecimalField(max_digits=5, decimal_places=2)
+    fourthScore = models.DecimalField(max_digits=5, decimal_places=2)
+    result = models.OneToOneField(Result, on_delete=models.CASCADE)
+
+
+class LoveLanguage(models.Model):
+    AFIRMATION = "AF"
+    SERVICE = "SE"
+    PRESENTS = "PE"
+    TIME = "TI"
+    TOUCH = "TO"
+    PREFERENCES = [
+        (AFIRMATION, "Afirmação"),
+        (SERVICE, "Serviço"),
+        (PRESENTS, "Presentes"),
+        (TIME, "Tempo"),
+        (TOUCH, "Toque"),
+    ]
+
+    first = models.CharField(choices=PREFERENCES, max_length=2)
+    second = models.CharField(choices=PREFERENCES, max_length=2)
+    third = models.CharField(choices=PREFERENCES, max_length=2)
+    fourth = models.CharField(choices=PREFERENCES, max_length=2)
+    fifth = models.CharField(choices=PREFERENCES, max_length=2)
+    firstScore = models.DecimalField(max_digits=5, decimal_places=2)
+    secondScore = models.DecimalField(max_digits=5, decimal_places=2)
+    thirdScore = models.DecimalField(max_digits=5, decimal_places=2)
+    fourthScore = models.DecimalField(max_digits=5, decimal_places=2)
+    fifthScore = models.DecimalField(max_digits=5, decimal_places=2)
+    result = models.OneToOneField(Result, on_delete=models.CASCADE)
+
+
+class Mbti(models.Model):
+    AIR = "AR"
+    EARTH = "EA"
+    FIRE = "FI"
+    WATER = "WA"
+    PERSONALITIES = [(AIR, "Ar"), (EARTH, "Terra"), (FIRE, "Fogo"), (WATER, "Água")]
+
+    first = models.CharField(choices=PERSONALITIES, max_length=2)
+    second = models.CharField(choices=PERSONALITIES, max_length=2)
+    third = models.CharField(choices=PERSONALITIES, max_length=2)
+    fourth = models.CharField(choices=PERSONALITIES, max_length=2)
+    firstScore = models.DecimalField(max_digits=5, decimal_places=2)
+    secondScore = models.DecimalField(max_digits=5, decimal_places=2)
+    thirdScore = models.DecimalField(max_digits=5, decimal_places=2)
+    fourthScore = models.DecimalField(max_digits=5, decimal_places=2)
+    result = models.OneToOneField(Result, on_delete=models.CASCADE)
