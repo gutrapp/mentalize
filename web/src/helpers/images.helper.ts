7f0f42ba6c9d88a9cd3f612b6a9bf061ab9e2d35
gutrapp <trappgustavo@gmail.com>
2023-11-05 11:12:18 -0300

    testing again

--- web/src/helpers/images.helper.ts
+++ web/src/helpers/images.helper.ts
@@ -1,9 +1,11 @@
 export const IMAGES = {
-  gold: "http://app.mentalize.com:8000/staticfiles/assets/images/gold.jpg",
+  gold: "http://app.mentallize.com:8000/staticfiles/assets/images/gold.jpg",
   bgMobile:
-    "http://app.mentalize.com:8000/staticfiles/assets/images/mobile.jpeg",
-  bg: "http://app.mentalize.com:8000/staticfiles/assets/images/11.jpeg",
-  frase: "http://app.mentalize.com:8000/staticfiles/assets/images/13.png",
-  fraseMobile: "http://app.mentalize.com:8000/staticfiles/assets/images/14.png",
-  loginBanner: "http://app.mentalize.com:8000/staticfiles/assets/images/4.jpeg",
+    "http://app.mentallize.com:8000/staticfiles/assets/images/mobile.jpeg",
+  bg: "http://app.mentallize.com:8000/staticfiles/assets/images/11.jpeg",
+  frase: "http://app.mentallize.com:8000/staticfiles/assets/images/13.png",
+  fraseMobile:
+    "http://app.mentallize.com:8000/staticfiles/assets/images/14.png",
+  loginBanner:
+    "http://app.mentallize.com:8000/staticfiles/assets/images/4.jpeg",
 };
