690847868650dc1da8aec9b849c0ba12d5ed29de
gutrapp <trappgustavo@gmail.com>
2023-11-05 11:06:05 -0300

    testing

--- api/config/settings.py
+++ api/config/settings.py
@@ -106,7 +106,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static"
+STATIC_URL = "staticfiles"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIR = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- web/src/helpers/images.helper.ts
+++ web/src/helpers/images.helper.ts
@@ -1,8 +1,9 @@
 export const IMAGES = {
-  gold: "http://app.mentalize.com:8000/static/assets/images/gold.jpg",
-  bgMobile: "http://app.mentalize.com:8000/static/assets/images/mobile.jpeg",
-  bg: "http://app.mentalize.com:8000/static/assets/images/11.jpeg",
-  frase: "http://app.mentalize.com:8000/static/assets/images/13.png",
-  fraseMobile: "http://app.mentalize.com:8000/static/assets/images/14.png",
-  loginBanner: "http://app.mentalize.com:8000/static/assets/images/4.jpeg",
+  gold: "http://app.mentalize.com:8000/staticfiles/assets/images/gold.jpg",
+  bgMobile:
+    "http://app.mentalize.com:8000/staticfiles/assets/images/mobile.jpeg",
+  bg: "http://app.mentalize.com:8000/staticfiles/assets/images/11.jpeg",
+  frase: "http://app.mentalize.com:8000/staticfiles/assets/images/13.png",
+  fraseMobile: "http://app.mentalize.com:8000/staticfiles/assets/images/14.png",
+  loginBanner: "http://app.mentalize.com:8000/staticfiles/assets/images/4.jpeg",
 };
