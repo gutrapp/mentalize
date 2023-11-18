6ac7d0726b8dcba9cc3c6906717b87ad7d4daedc
gutrapp <trappgustavo@gmail.com>
2023-11-18 10:02:22 -0300

    fix: api url

--- web/src/api/api.config.ts
+++ web/src/api/api.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: "http://app.mentallize.com:8000/api/",
 });
 
 api.defaults.xsrfCookieName = "csrftoken";
